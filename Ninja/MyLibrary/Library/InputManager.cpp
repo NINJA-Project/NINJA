a47a6858a92698c4c7e123eb6c7d4274cac6fe77
ガレリア\哲良 <shibatatetsuo0430@gmail.com>
2017-06-03 00:21:23 +0900

    ライブラリのコミット漏れ

---
+++ Ninja/MyLibrary/Library/InputManager.cpp
@@ -0,0 +1,37 @@
+﻿/**
+ * @file	InputManager.cpp
+ * @breif	入力機器の管理をしているクラス実装
+ * @author	shibata
+ */
+
+#include "InputManager.h"
+#include "InputKey.h"
+#include "Define.h"
+#include "DebugSystem.h"
+
+InputManager::InputManager() : 
+m_pInputKey(New InputKey)
+{
+	MyAssert(m_pInputKey, "NULLが入っています");
+}
+
+InputManager::~InputManager()
+{
+	SafeDelete(m_pInputKey);
+}
+
+void InputManager::UpdateDI()
+{
+	m_pInputKey->UpdateKey();
+}
+
+void InputManager::UpdateKey()
+{
+	m_pInputKey->UpdateKey();
+}
+
+InputManager::KeyState InputManager::CheckKey(int dik_)
+{
+	KeyState state = static_cast<InputManager::KeyState>(m_pInputKey->CheckKey(dik_));
+	return state;
+}
