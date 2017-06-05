45cbf97f1409e675bb99f67ac86261a05c259ba5
ガレリア\哲良 <shibatatetsuo0430@gmail.com>
2017-06-06 04:01:42 +0900

    マップ関係のクラスを追加 ※実装は途中段階のもの

---
+++ Ninja/Ninja/Map/Map.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file	Map.cpp
+ * @breif	マップクラス実装
+ * @author	shibata
+ */
+
+#include "Map.h"
+#include <stdio.h>
+
+Map::Map()
+{
+
+}
+
+Map::~Map()
+{
+
+}
+
+void Map::Control()
+{
+
+}
+
+void Map::Draw()
+{
+
+}
+
+void Map::LoadCSV(const char* filePath_)
+{
+	FILE* fp;
+}
---
+++ Ninja/Ninja/Map/MapManager.h
@@ -0,0 +1,20 @@
+/**
+ * @file	MapManager.h
+ * @breif	マップを管理するクラスヘッダ
+ * @author	shibata
+ */
+
+#ifndef MAPMANAGER_H
+#define MAPMANAGER_H
+
+class MapManager
+{
+public:
+	MapManager();
+	~MapManager();
+
+	void Control();
+	void Draw();
+};
+
+#endif // !MAPMANAGER_H
