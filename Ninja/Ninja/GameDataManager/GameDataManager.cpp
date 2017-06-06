aaa3c2a61b09764ffc0893d7dc966dfb6463723b
ガレリア\哲良 <shibatatetsuo0430@gmail.com>
2017-06-07 03:53:21 +0900

    データマネージャーの追加

---
+++ Ninja/Ninja/GameDataManager/GameDataManager.cpp
@@ -0,0 +1,17 @@
+/**
+* @file		GameDataManager.h
+* @breif	ゲーム上のデータを管理するクラス実装
+* @author	shibata
+*/
+
+#include "GameDataManager.h"
+
+GameDataManager::GameDataManager()
+{
+
+}
+
+GameDataManager::~GameDataManager()
+{
+
+}
--- Ninja/Ninja/GameDataManager/GameDataManager.h
+++ Ninja/Ninja/GameDataManager/GameDataManager.h
@@ -1,1 +1,29 @@
+/**
+ * @file	GameDataManager.h
+ * @breif	ゲーム上のデータを管理するクラスヘッダ
+ * @author	shibata
+ */
 
+#ifndef GAMEDATAMANAGER_H
+#define GAMEDATAMANAGER_H
+
+/* 
+他のクラスから値などを取得したい場合は
+このクラスを経由して取得すること
+基本インライン関数で実装すること
+*/
+#include <Library\Singleton.h>
+
+class GameDataManager : public Singleton<GameDataManager>
+{
+private:
+	friend Singleton<GameDataManager>;
+	GameDataManager();
+	virtual ~GameDataManager();
+
+public:
+
+};
+
+
+#endif // !GAMEDATAMANAGER_H
