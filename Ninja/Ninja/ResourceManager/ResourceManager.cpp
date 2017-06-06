0a21dda5e31b2a9e75ae8aa699a0ea09f67ed713
ガレリア\哲良 <shibatatetsuo0430@gmail.com>
2017-06-07 03:55:07 +0900

    リソース読み込みクラスを追加

---
+++ Ninja/Ninja/ResourceManager/ResourceManager.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file	ResourceManager.cpp
+ * @breif	ゲーム上で必要なリソース読み込みクラス実装
+ * @author	shibata
+ */
+
+
+#include "ResourceManager.h"
+#include "../Scene/Scene.h"
+#include <Library.h>
+
+ResourceManager::ResourceManager() :
+m_rLibrary(Library::Instance())
+{
+
+}
+
+ResourceManager::~ResourceManager()
+{
+
+}
+
+void ResourceManager::TexLoader(Scene::ID currentID_)
+{
+	switch (currentID_)
+	{
+	case Scene::ID::LOGO_SCENE:
+		// ここに読み込むテクスチャを書く
+		// 新規で読み込む際はhの対応するenumにテクスチャIDを追加すること
+		break;
+	case Scene::ID::TITLE_SCENE:
+		// ここに読み込むテクスチャを書く
+		// 新規で読み込む際はhの対応するenumにテクスチャIDを追加すること
+
+		break;
+	case Scene::ID::MAIN_SCENE:
+		// ここに読み込むテクスチャを書く
+		// 新規で読み込む際はhの対応するenumにテクスチャIDを追加すること
+		m_rLibrary.LoadTextureFile(MAP_CHIP1, "Resource/Texture/MapChip.png");
+
+		break;
+	}
+}
+
+void ResourceManager::CSVLoader()
+{
+
+}
---
+++ Ninja/Ninja/ResourceManager/ResourceManager.h
@@ -0,0 +1,55 @@
+/**
+ * @file	ResourceManager.h
+ * @breif	ゲーム上で必要なリソース読み込みクラスヘッダ
+ * @author	shibata
+ */
+
+#ifndef RESOURCEMANAGER_H
+#define RESOURCEMANAGER_H
+
+#include <Library\Singleton.h>
+#include "../Scene/Scene.h"
+
+class Library;
+
+class ResourceManager : public Singleton<ResourceManager>
+{
+private:
+	friend Singleton<ResourceManager>;
+	ResourceManager();
+	virtual ~ResourceManager();
+
+public:
+#pragma region ロゴシーンのテクスチャID
+	enum LogoTex
+	{
+		LOGO
+	};
+#pragma endregion
+#pragma region タイトルシーンのテクスチャID
+	enum TitleTex
+	{
+		TITLE_LOGO,
+		BUTTON,
+		START_LOGO,
+		HOWTOPLAY_LOGO
+	};
+#pragma endregion
+#pragma region メインシーンのテクスチャID
+	enum MainTex
+	{
+		MAP_CHIP1,
+		MAP_CHIP2,
+		MAP_CHIP3,
+	};
+#pragma endregion
+
+public:
+	void TexLoader(Scene::ID currentID_);
+	void CSVLoader();
+
+private:
+	Library&	m_rLibrary;
+};
+
+#endif // !RESOURCEMANAGER_H
