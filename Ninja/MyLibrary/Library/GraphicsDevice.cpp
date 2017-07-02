98f33a94a7530a97efe754dcde71229507048c66
ガレリア\哲良 <shibatatetsuo0430@gmail.com>
2017-07-03 00:59:27 +0900

    Libraryのリファクタリング
    DirectX9クラスの名前をGraphicsDeviceに変更
    また、関数名を変更

---
+++ Ninja/MyLibrary/Library/GraphicsDevice.cpp
@@ -0,0 +1,144 @@
+﻿/**
+ * @file	GraphicsDevice.cpp
+ * @breif	DirectXの初期化クラス実装
+ * @author	shibata
+ */
+
+#include "GraphicsDevice.h"
+#include "CommoSystem.h"
+
+GraphicsDevice::GraphicsDevice() :
+m_pDirect3D(nullptr),
+m_pD3Device(nullptr)
+{
+
+}
+
+GraphicsDevice::~GraphicsDevice()
+{
+	SafeRelease(m_pD3Device);
+	SafeRelease(m_pDirect3D);
+}
+
+void GraphicsDevice::Initialize(HWND hwnd_, int clientWidth_, int clientHeight_, bool isFullScreen_, bool is3D_)
+{
+	m_pDirect3D = Direct3DCreate9(D3D_SDK_VERSION);
+	MyAssert(m_pDirect3D, "初期化に失敗しました");
+
+	// ウィンドウタイプの設定
+	{
+		// ディスプレイモード
+		D3DDISPLAYMODE d3ddm;
+		m_pDirect3D->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &d3ddm);
+
+		// 最終的なウィンドウパラメーター
+		D3DPRESENT_PARAMETERS d3dpp;
+		ZeroMemory(&d3dpp, sizeof(D3DPRESENT_PARAMETERS));
+
+		if (isFullScreen_)
+		{
+			// フルスクリーンパラメーター
+			D3DPRESENT_PARAMETERS fullScreenPrm;
+			ZeroMemory(&fullScreenPrm, sizeof(D3DPRESENT_PARAMETERS));
+			fullScreenPrm.BackBufferWidth		= clientWidth_;
+			fullScreenPrm.BackBufferHeight		= clientHeight_;
+			fullScreenPrm.BackBufferFormat		= d3ddm.Format;
+			fullScreenPrm.BackBufferCount		= 1;
+			fullScreenPrm.SwapEffect			= D3DSWAPEFFECT_DISCARD;
+			fullScreenPrm.hDeviceWindow			= hwnd_;
+			fullScreenPrm.Windowed				= FALSE;
+			fullScreenPrm.PresentationInterval	= D3DPRESENT_INTERVAL_DEFAULT;
+			d3dpp								= fullScreenPrm;
+		}
+		else
+		{
+			// ウィンドウスクリーンパラメーター
+			D3DPRESENT_PARAMETERS windowScreenPrm;
+			ZeroMemory(&windowScreenPrm, sizeof(D3DPRESENT_PARAMETERS));
+			windowScreenPrm.BackBufferFormat	= d3ddm.Format;
+			windowScreenPrm.BackBufferCount		= 1;
+			windowScreenPrm.SwapEffect			= D3DSWAPEFFECT_DISCARD;
+			windowScreenPrm.Windowed			= TRUE;
+			d3dpp								= windowScreenPrm;
+		}
+		
+		m_pDirect3D->CreateDevice(
+			D3DADAPTER_DEFAULT,
+			D3DDEVTYPE_HAL,
+			hwnd_,
+			D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+			&d3dpp,
+			&m_pD3Device);
+		MyAssert(m_pD3Device, "初期化に失敗しました");
+	}
+
+	if (is3D_)
+	{
+		Initialize3DRendering();
+	}
+	else
+	{
+		InitializeRendering();
+	}
+
+}
+
+void GraphicsDevice::SetFVF(DWORD fvf_)
+{
+	m_pD3Device->SetFVF(fvf_);
+}
+
+void GraphicsDevice::RenderStarting()
+{
+	m_pD3Device->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0x00, 0x00, 0x00), 1.0, 0);
+
+	m_pD3Device->BeginScene();
+}
+
+void GraphicsDevice::RenderEnding()
+{
+	m_pD3Device->EndScene();
+
+	m_pD3Device->Present(NULL, NULL, NULL, NULL);
+}
+
+#pragma region privateメンバ関数
+void GraphicsDevice::InitializeRendering()
+{
+	m_pD3Device->SetRenderState(D3DRS_ALPHABLENDENABLE, true);
+	m_pD3Device->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	m_pD3Device->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+	m_pD3Device->SetRenderState(D3DRS_DIFFUSEMATERIALSOURCE, D3DMCS_COLOR1);
+
+	m_pD3Device->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+	m_pD3Device->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);
+
+	m_pD3Device->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
+
+	m_pD3Device->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
+	m_pD3Device->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_DIFFUSE);
+
+	m_pD3Device->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
+}
+
+void GraphicsDevice::Initialize3DRendering()
+{
+	m_pD3Device->SetRenderState(D3DRS_ALPHABLENDENABLE, true);
+	m_pD3Device->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	m_pD3Device->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+	m_pD3Device->SetRenderState(D3DRS_DIFFUSEMATERIALSOURCE, D3DMCS_COLOR1);
+
+	m_pD3Device->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+	m_pD3Device->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);
+
+	m_pD3Device->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
+
+	m_pD3Device->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
+	m_pD3Device->SetTextureStageState(0, D3DTSS_ALPHAARG2, D3DTA_DIFFUSE);
+
+	m_pD3Device->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
+
+	m_pD3Device->SetRenderState(D3DRS_ZENABLE, TRUE);			// Zバッファー処理を有効に(3D)
+	m_pD3Device->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);	// カリングする
+}
+#pragma endregion
---
+++ Ninja/MyLibrary/Library/GraphicsDevice.h
@@ -0,0 +1,71 @@
+﻿/**
+ * @file	GraphicsDevice.h
+ * @breif	DirectXの初期化クラスヘッダ
+ * @author	shibata
+ */
+
+#ifndef GRAPHICSDEVICE_H
+#define GRAPHICSDEVICE_H
+
+#include <d3dx9.h>
+#include "Singleton.h"
+
+class GraphicsDevice : public Singleton<GraphicsDevice>
+{
+private:
+	friend Singleton<GraphicsDevice>;
+	/**コンストラクタ*/
+	GraphicsDevice();
+	/**デストラクタ*/
+	~GraphicsDevice();
+
+public:
+	/**
+	 * 初期化関数
+	 * @param [in]	hwnd_			ウィンドウハンドル
+	 * @param [in]	clientWidth_	画面横幅のサイズ
+	 * @param [in]	clientHeight_	画面縦幅のサイズ
+	 * @param [in]	isFullScreen_	フルスクリーンにするかどうか		@note デフォルト値はLibraryクラスで false に設定している
+	 * @param [in]	is3D_			3Dを使用するかどうか				@note デフォルト値はLibraryクラスで false に設定している
+	 */
+	void Initialize(HWND hwnd_, int clientWidth_, int clientHeight_, bool isFullScreen_, bool is3D_);
+
+	/**
+	 * 頂点フォーマット設定関数
+	 * @param [in] fvf_ 頂点フォーマットの設定			@note デフォルトはLibraryクラスで D3DFVF_CUSTOMVERTEX に設定している
+	 */
+	void SetFVF(DWORD fvf_);
+
+	/**描画開始関数*/
+	void RenderStarting();
+
+	/**描画終了関数*/
+	void RenderEnding();
+
+	/**
+	 * デバイス取得関数
+	 * @return	m_pD3Device	デバイス情報
+	 */
+	LPDIRECT3DDEVICE9 GetDevice() const
+	{
+		return m_pD3Device;
+	}
+
+private:
+#pragma region メンバ関数
+	/**描画初期化関数*/
+	void InitializeRendering();
+	/**
+	* 描画初期化関数				@note 3Dを描画するときに使用
+	*/
+	void Initialize3DRendering();
+#pragma endregion
+
+#pragma region メンバ変数
+private:
+	LPDIRECT3D9			m_pDirect3D;	//!< DirectXのインターフェイス
+	LPDIRECT3DDEVICE9	m_pD3Device;	//!< DirectXのデバイス
+#pragma endregion
+};
+
+#endif // !GRAPHICSDEVICE_H
