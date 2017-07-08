cffede11e8de9777dca388a23653b0fcf45decd8
ガレリア\哲良 <shibatatetsuo0430@gmail.com>
2017-07-09 03:16:20 +0900

    #30の対応
    XFileManagerとその配下のコメント修正

--- Ninja/MyLibrary/Library/XFileManager.h
+++ Ninja/MyLibrary/Library/XFileManager.h
@@ -20,14 +20,14 @@ public:
 
 	/**
 	* Xファイルの読み込み関数
-	* @param [in]	index_		Xファイルの配列番号
+	* @param [in]	index_		読み込むXファイルの配列番号
 	* @param [in]	filePath_	Xファイルのパス
 	*/
 	void Load(int index_, const char* filePath_);
 
 	/**
 	 * Xファイルの描画関数
-	 * @param [in] index_
+	 * @param [in] index_		描画するXファイルの配列番号(読み込んだ配列番号と同じ)
 	 */
 	void DrawXFile(int index_);
 
