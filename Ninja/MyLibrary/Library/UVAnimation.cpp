a1b9bd6d56baaeb1f813b5d9068914abf55378be
s-2013000001366\share <shibatatetsuo0430@gmail.com>
2017-07-14 19:07:23 +0900

    アニメーションに必要なクラスを追加

---
+++ Ninja/MyLibrary/Library/UVAnimation.cpp
@@ -0,0 +1,15 @@
+/**
+* @file		UVAnimation.cpp
+* @breif	UVアニメーションクラス実装
+* @author	shibata
+*/
+
+#include "UVAnimation.h"
+
+UVAnimation::UVAnimation()
+{
+}
+
+UVAnimation::~UVAnimation()
+{
+}
---
+++ Ninja/MyLibrary/Library/UVAnimation.h
@@ -0,0 +1,19 @@
+/**
+ * @file	UVAnimation.h
+ * @breif	UVアニメーションクラスヘッダ
+ * @author	shibata
+ */
+
+#ifndef UVANIMATION_H
+#define UVANIMATION_H
+
+class UVAnimation
+{
+public:
+	UVAnimation();
+	~UVAnimation();
+
+
+};
+
+#endif // !UVANIMATION_H
