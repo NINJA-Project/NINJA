8aa254a1725f0c431b80303765b8b5d3632b4db9
s-2013000001366\share <shibatatetsuo0430@gmail.com>
2017-07-18 15:48:57 +0900

    アニメーションに必要そうな変数を追加

--- Ninja/MyLibrary/Library/UVAnimation.h
+++ Ninja/MyLibrary/Library/UVAnimation.h
@@ -7,6 +7,9 @@
 #ifndef UVANIMATION_H
 #define UVANIMATION_H
 
+#include "fRect.h"
+#include <vector>
+
 class UVAnimation
 {
 public:
@@ -14,6 +17,23 @@ public:
 	~UVAnimation();
 
 
+#pragma region メンバ変数
+private:
+#pragma region アニメーションデータ構造体
+	struct AnimaData
+	{
+		fRect	m_texUV;
+		int		m_flameCount;
+
+		AnimaData() :
+			m_texUV(0.0f, 0.0f, 0.0f, 0.0f),
+			m_flameCount(0)
+		{}
+	};
+#pragma endregion
+	std::vector<AnimaData>	m_animationData;
+	std::vector<int>		m_animationNumber;
+#pragma endregion
 };
 
 #endif // !UVANIMATION_H
